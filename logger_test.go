94455b7431497625329071d8f8b952dc55457bcf
elizarpif <liza.pif@mail.ru>
2022-10-06 21:48:47 +0200

    added test

---
+++ logger_test.go
@@ -0,0 +1,35 @@
+package zaplogger
+
+import (
+	"context"
+	"testing"
+)
+
+func TestWithTraceId(t *testing.T) {
+	type args struct {
+		ctx     context.Context
+		traceID string
+	}
+	tests := []struct {
+		name string
+		args args
+		want context.Context
+	}{
+		{
+			name: "check adding value to ctx",
+			args: args{
+				context.Background(),
+				"some_trace",
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := WithTraceId(tt.args.ctx, tt.args.traceID)
+
+			log := addValuesFromCtx(ctx)
+			log.Info("check")
+		})
+	}
+}
